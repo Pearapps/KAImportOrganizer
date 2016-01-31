3ee8c8fd1ff7f3d1c1fa88da626c6ca3220f3ce2
kenny <pearapps@gmail.com>
2016-01-30 21:30:19 -0500

    Adding a more complicated test

--- ImportFixerTests/KAImportStringTransformerTests.m
+++ ImportFixerTests/KAImportStringTransformerTests.m
@@ -77,6 +77,52 @@
     XCTAssert([projectedOutcome isEqualToString:transformedString]);
 }
 
+- (void)testInsertedNewLinesBetweenACoupleTypesWithEachMultipleImports {
+    NSArray *originalImports = @[
+                                 [[KAImportStatement alloc] initWithImportString:@"#import \"Kenny.h\"\n"],
+                                 [[KAImportStatement alloc] initWithImportString:@"@import Foundation;\n"],
+                                 [[KAImportStatement alloc] initWithImportString:@"@import UIKit;\n"],
+                                 [[KAImportStatement alloc] initWithImportString:@"#import <Foundation/Foundation.h>\n"],
+                                 [[KAImportStatement alloc] initWithImportString:@"#import <AppKit/AppKit>\n"],
+                                 [[KAImportStatement alloc] initWithImportString:@"#import \"Defines.h\"\n"],
+                                 ];
+    
+    NSArray *sortedImports = @[
+                               [[KAImportStatement alloc] initWithImportString:@"@import Foundation;\n"],
+                               [[KAImportStatement alloc] initWithImportString:@"@import UIKit;\n"],
+                               [[KAImportStatement alloc] initWithImportString:@"#import <AppKit/AppKit>\n"],
+                               [[KAImportStatement alloc] initWithImportString:@"#import <Foundation/Foundation.h>\n"],
+                               [[KAImportStatement alloc] initWithImportString:@"#import \"Defines.h\"\n"],
+                               [[KAImportStatement alloc] initWithImportString:@"#import \"Kenny.h\"\n"]
+                               ];
+    
+    NSString *contents = @"//Some stuff\n"
+    "// hello\n"
+    "#import \"Kenny.h\"\n"
+    "@import Foundation;\n"
+    "@import UIKit;\n"
+    "#import <Foundation/Foundation.h>\n"
+    "#import <AppKit/AppKit>\n"
+    @"#import \"Defines.h\"\n";
+    
+    NSString *transformedString = [[[KAImportStringTransformer alloc] initWithOriginalImports:originalImports sortedImportStatements:sortedImports originalContents:contents numberOfNewlines:0 insertsNewLinesInBetweenTypes:YES] transformedString];
+    
+    NSString *projectedOutcome = @"//Some stuff\n"
+    "// hello\n"
+    "@import Foundation;\n"
+    "@import UIKit;\n"
+    "\n"
+    "#import <AppKit/AppKit>\n"
+    "#import <Foundation/Foundation.h>\n"
+    "\n"
+    "#import \"Defines.h\"\n"
+    "#import \"Kenny.h\"\n";
+    
+    
+    XCTAssert([projectedOutcome isEqualToString:transformedString]);
+}
+
+
 - (void)testBasicImportCreationSwift {
     NSArray *originalImports = @[
                                  [[KAImportStatement alloc] initWithImportString:@"import Kenny\n"],
